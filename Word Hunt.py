435d0e87b8feeeffff2ef3348554fa04100e0db2
tracywai <83553604+tracywai@users.noreply.github.com>
2021-07-15 13:39:59 -0400

    Update Word Hunt.py

--- Word Hunt.py
+++ Word Hunt.py
@@ -1,15 +1,1 @@
-class WordHuntBoard:
 
-    def __init__(self):
-        self.board = ['_']* 16
-
-    def print_board(self):
-        for i in range(0,len(self.board),4):
-            print (' '.join(self.board[i:i+4]))
-
-    def shake(self):
-        pass
-    
-b = WordHuntBoard()
-
-b.print_board()
